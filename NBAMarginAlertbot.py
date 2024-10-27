50f98a06e393f607766ed6bb0d596366ae349dc3
Yared W <yaredwondatir77@gmail.com>
2024-10-28 01:29:06 +0300

    new chapter

--- NBAMarginAlertbot.py
+++ NBAMarginAlertbot.py
@@ -5,7 +5,7 @@ import time
 from datetime import datetime, timedelta
 from dotenv import load_dotenv
 import json
-from flask import Flask, request
+
 load_dotenv()
 
 # Get API tokens from environment variables
@@ -17,22 +17,6 @@ team_data = {}
 bot = telebot.TeleBot(BOT_TOKEN)
 alert_margin = 10 
 
-app = Flask(__name__)
-
-# Define a route for webhook updates
-@app.route(f'/{BOT_TOKEN}', methods=['POST'])
-def webhook():
-    update = telebot.types.Update.de_json(request.get_json(force=True))
-    bot.process_new_updates([update])
-    return 'OK', 200
-
-# Example of setting the webhook
-@app.route('/set_webhook', methods=['GET', 'POST'])
-def set_webhook():
-    #webhook_url = f"https://project-9e2j.onrender.com/{BOT_TOKEN}"  # Replace <YOUR_RENDER_APP_URL> with Render URL
-    webhook_url = f"https://api.telegram.org/bot{BOT_TOKEN}/setWebhook?url=https://project-9e2j.onrender.com/{BOT_TOKEN}"  # Replace <YOUR_RENDER_APP_URL> with Render URL
-    success = bot.set_webhook(url=webhook_url)
-    return 'Webhook setup' if success else 'Webhook setup failed', 200
 
 @bot.message_handler(commands=['start'])
 def start(message):
@@ -311,9 +295,5 @@ def monitor_games(chat_id, team_names):
 
 
 print("bot runnin...")
-
-# Run the Flask app
-if __name__ == "__main__":
-    #app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
-    from waitress import serve
-    serve(app, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+# Webhook route to receive updates
+bot.polling()
