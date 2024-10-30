512bbc51cf4a6e2fbbd5b21dde1493a7c6bfddc8
Yared W <yaredwondatir77@gmail.com>
2024-10-30 13:40:45 +0300

    get flask its own  file

--- NBAMarginAlertbot.py
+++ NBAMarginAlertbot.py
@@ -5,10 +5,7 @@ import time
 from datetime import datetime
 from dotenv import load_dotenv
 import json
-from flask import Flask, request
-#from gevent.pywsgi import WSGIServer
-from waitress import serve
-import logging
+
 load_dotenv()
 
 # Get API tokens from environment variables
@@ -20,23 +17,7 @@ team_data = {}
 bot = telebot.TeleBot(BOT_TOKEN)
 alert_margin = 10 
 
-logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(message)s')
-app = Flask(__name__)
 
-# Define a route for webhook updates
-@app.route(f'/{BOT_TOKEN}', methods=['POST'])
-def webhook():
-    update = telebot.types.Update.de_json(request.get_json(force=True))
-    bot.process_new_updates([update])
-    return 'OK', 200
-
-# Example of setting the webhook
-@app.route('/set_webhook', methods=['GET', 'POST'])
-def set_webhook():
-    #webhook_url = f"https://project-9e2j.onrender.com/{BOT_TOKEN}"  # Replace <YOUR_RENDER_APP_URL> with Render URL
-    webhook_url = f"https://api.telegram.org/bot{BOT_TOKEN}/setWebhook?url=https://project-9e2j.onrender.com/{BOT_TOKEN}"  # Replace <YOUR_RENDER_APP_URL> with Render URL
-    success = bot.set_webhook(url=webhook_url)
-    return 'Webhook setup' if success else 'Webhook setup failed', 200
 
 @bot.message_handler(commands=['start'])
 def start(message):
@@ -317,9 +298,3 @@ def monitor_games(chat_id, team_names):
 print("bot runnin...")
 #bot.polling()
 # Run the Flask app
-if __name__ == "__main__":
-    #app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
-    
-    serve(app, host='0.0.0.0', port=5000)
-    #http_server = WSGIServer(('', 5000), app)
-    #http_server.serve_forever()
